68a345809056c3c90480f69d21497cd0adfe101e
Richymaestro <xxxx@yyy.com>
2023-05-08 09:38:49 +0100

    Refactor folder transactionbuilder for readability Fixes #13

--- transaction_builder/cli.py
+++ transaction_builder/cli.py
@@ -1,13 +1,13 @@
-"""Console script for transactionbuilder."""
+"""Console script for transaction_builder."""
 import sys
 import click
 
 
 @click.command()
 def main(args=None):
-    """Console script for transactionbuilder."""
+    """Console script for transaction_builder."""
     click.echo("Replace this message by putting your code into "
-               "transactionbuilder.cli.main")
+               "transaction_builder.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
