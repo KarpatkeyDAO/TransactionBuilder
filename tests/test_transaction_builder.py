994ee6a012e20d21cffc16c28870b2c0d7a820f8
Richymaestro <xxxx@yyy.com>
2023-05-08 11:05:59 +0100

    Add test_it to main file + test Fixes #21

--- tests/test_transaction_builder.py
+++ tests/test_transaction_builder.py
@@ -12,7 +12,7 @@ from transaction_builder.util.enums import Chain
 
 BLOCKCHAIN = Chain.GNOSIS.value
 FUNCTION_ARGS = ['0x7f90122BF0700F9E7e1F688fe926940E8839F353',100000000]
-FUNCTION_NAME = ['approve']
+FUNCTION_NAME = 'approve'
 CONTRACT_ADDRESS = '0x4ECaBa5870353805a9F068101A40E0f32ed605C6'
 CONTRACT_ABI = '[{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},\
                  {"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"result","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]'
@@ -21,7 +21,7 @@ ROLES_MOD_ADDRESS = '0xB6CeDb9603e7992A5d42ea2246B3ba0a21342503'
 ACCOUNT = '0x7e19DE37A31E40eec58977CEA36ef7fB70e2c5CD'
 
 def test_test_it():
-    data = main.test_it(BLOCKCHAIN,FUNCTION_ARGS,FUNCTION_NAME,CONTRACT_ADDRESS,CONTRACT_ADDRESS,ROLE,ACCOUNT,ROLES_MOD_ADDRESS)
+    data = main.test_it(BLOCKCHAIN,FUNCTION_ARGS,FUNCTION_NAME,CONTRACT_ADDRESS,CONTRACT_ABI,ROLE,ACCOUNT,ROLES_MOD_ADDRESS)
     assert data == True
 
 # @pytest.fixture
